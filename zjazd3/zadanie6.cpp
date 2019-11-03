80a4cdc7dd6efa82868fc272f468f69a447c1e75
Benedykt Borowski <s20685@pjwstk.edu.pl>
2019-11-03 18:33:14 +0100

    zadanie6

---
+++ zjazd3/zadanie6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int woda[3][3];
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            cin >> woda[i][j];
+        }
+    }
+
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            cout << " " <<woda[i][j];
+
+        }
+        cout <<endl;
+    }
+    return 1;
+}
+
